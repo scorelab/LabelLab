32953b804fa1666ad7a5e284575b63c0ca6f2f63
shreya2feb <shreya2feb@gmail.com>
2019-02-05 19:44:39 +0530

    rest started

--- Mobile_App/backend/index.js
+++ Mobile_App/backend/index.js
@@ -1,13 +1,16 @@
-var http = require("http");
-http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
-}).listen(8081);
+var express = require('express');
+var app = express();
+var fs = require("fs");
 
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+app.get('/listUsers', function (req, res) {
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      console.log( data );
+      res.end( data );
+   });
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://127.0.0.1:%s", port)
+})
---
+++ Mobile_App/backend/users.json
@@ -0,0 +1,22 @@
+{
+   "user1" : {
+      "name" : "mahesh",
+      "password" : "password1",
+      "profession" : "teacher",
+      "id": 1
+   },
+   
+   "user2" : {
+      "name" : "suresh",
+      "password" : "password2",
+      "profession" : "librarian",
+      "id": 2
+   },
+   
+   "user3" : {
+      "name" : "ramesh",
+      "password" : "password3",
+      "profession" : "clerk",
+      "id": 3
+   }
+}
