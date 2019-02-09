9e51a8cbd572e1d72320787f997763009015acea
Udesh Kumarasinghe <mail@udeshuk.me>
2019-02-09 20:51:57 +0530

    Implemented image upload in backend

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fileUpload = require('express-fileupload');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var classifyRouter = require('./routes/classify');
 
 var app = express();
 
@@ -11,8 +12,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(fileUpload());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/classify', classifyRouter);
 
 module.exports = app;
