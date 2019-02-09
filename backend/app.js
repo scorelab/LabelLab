cb4500dfd6459733740fcf0f440eccb327c9682d
Udesh Kumarasinghe <mail@udeshuk.me>
2019-02-09 10:48:29 +0530

    Initialize basic project structure

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
