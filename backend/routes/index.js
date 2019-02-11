56f9ccf2caef66b79b5b075d4053cf57d0dbaf53
Udesh Kumarasinghe <mail@udeshuk.me>
2019-02-12 00:47:05 +0530

    Removed rendering

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send({});
 });
 
 module.exports = router;
