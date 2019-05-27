db4659b081c701794128183667dd23cc04e36e7c
Tushar Varshney <tvarshney@me.iitr.ac.in>
2019-05-28 01:02:19 +0530

    feat: login/register implemented, validations added, config added, passport.js installed and implemented

--- labellab-server/app.js
+++ labellab-server/app.js
@@ -1,25 +1,38 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var cookieParser = require("cookie-parser");
+var mongoose = require("mongoose");
+var logger = require("morgan");
+let passport = require("passport");
+let cors = require("cors");
+let path = require("path");
 
-var indexRouter = require('./routes/routes');
+var indexRouter = require("./routes/routes");
+let config = require("./config/db_uri");
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
+app.use(logger("dev"));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("static", express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+
+// Passport Config
+require("./config/passport").passport;
+
+// Connect to MongoDB
+mongoose
+  .connect(config.mongoURI, {
+    promiseLibrary: require("bluebird"),
+    useNewUrlParser: true
+  })
+  .then(() => console.log("MongoDB connected successfully!"))
+  .catch(err => console.error(err));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -30,11 +43,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
