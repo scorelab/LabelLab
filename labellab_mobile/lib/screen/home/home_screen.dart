64f5d90779153de56c2a07b4b68f21d88675a7c3
Udesh Kumarasinghe <mail@udeshuk.me>
2019-05-29 01:26:06 +0530

    Refactored starter project code

---
+++ labellab_mobile/lib/screen/home/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("LabelLab"),
+        ),
+        body: Placeholder(),
+      ),
+    );
+  }
+}
