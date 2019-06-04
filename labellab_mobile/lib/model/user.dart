3a540c47e7ad1c93766af3ffca1e030d854a36e8
Udesh Kumarasinghe <mail@udeshuk.me>
2019-06-05 02:45:27 +0530

    Add auth user model

--- labellab_mobile/lib/model/user.dart
+++ labellab_mobile/lib/model/user.dart
@@ -1,9 +1,11 @@
-class User {
+import 'package:labellab_mobile/model/auth_user.dart';
+
+class User extends AuthUser{
   String name;
   String username;
   String email;
   String password;
   DateTime createdAt;
 
-  User({this.name, this.username, this.email, this.password, this.createdAt});
+  User({this.name, this.username, this.email, this.password, this.createdAt}) : super(username, password);
 }
