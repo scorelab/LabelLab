856e95e5b1b0af034ffb7e94f3d444205f4b9d26
Udesh Kumarasinghe <mail@udeshuk.me>
2019-06-05 02:46:34 +0530

    Implement login screen ui

--- labellab_mobile/lib/routing/route_handler.dart
+++ labellab_mobile/lib/routing/route_handler.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:fluro/fluro.dart';
+import 'package:labellab_mobile/screen/login/login_screen.dart';
 import 'package:labellab_mobile/screen/main_screen.dart';
 
 var mainHandler = Handler(
     handlerFunc: (BuildContext context, Map<String, List<String>> params) {
   return MainScreen();
+});
+
+var loginHandler = Handler(
+    handlerFunc: (BuildContext context, Map<String, List<String>> params) {
+  return LoginScreen();
 });
