dcf53b3e0c133755da1e13db73c81d9bc77d95f9
Udesh Kumarasinghe <mail@udeshuk.me>
2019-06-06 03:45:38 +0530

    Integrated sign in with remote api

--- labellab_mobile/lib/state/auth_state.dart
+++ labellab_mobile/lib/state/auth_state.dart
@@ -1,26 +1,38 @@
 import 'package:flutter/foundation.dart';
+import 'package:labellab_mobile/data/repository.dart';
+import 'package:labellab_mobile/model/auth_user.dart';
 import 'package:labellab_mobile/model/user.dart';
 
-class AuthState with ChangeNotifier{
+class AuthState with ChangeNotifier {
+  final Respository _respository = Respository();
 
+  // State
   User user;
 
   AuthState() {
     // TODO - Load cached user, check the validity and assign to current user
-    user = User(
-      name: "Udesh Kumarasinghe",
-      username: "udeshuk",
-      email: "mail@udesh.xyz",
-    );
+    // user = User(
+    //   name: "Udesh Kumarasinghe",
+    //   username: "udeshuk",
+    //   email: "mail@udesh.xyz",
+    // );
     notifyListeners();
   }
 
-  void signin(String username, String password) {
-    // TODO - Authenticate user with the backend and cache it
+  Future<bool> signin(AuthUser user) {
+    return _respository.login(user).then((response) {
+      print("Success: " + response.token);
+      this.user = User(
+        username: "udeshuk",
+        email: "mail@udesh.xyz",
+        name: "Udesh Kumarasinghe",
+      );
+      notifyListeners();
+      return response.success;
+    });
   }
 
   void signout() {
     // TODO - Implement sign out login
   }
-
 }
