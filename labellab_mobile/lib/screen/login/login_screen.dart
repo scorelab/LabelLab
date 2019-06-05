9b1c8fd47a827fb4960e8d4642e640220a383006
Udesh Kumarasinghe <mail@udeshuk.me>
2019-06-05 10:08:19 +0530

    SSO Sign in UI, sign up routing

--- labellab_mobile/lib/screen/login/login_screen.dart
+++ labellab_mobile/lib/screen/login/login_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:labellab_mobile/model/auth_user.dart';
+import 'package:labellab_mobile/routing/application.dart';
 import 'package:labellab_mobile/widgets/label_text_field.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 class LoginScreen extends StatefulWidget {
   LoginScreen({Key key}) : super(key: key);
@@ -25,66 +27,84 @@ class _LoginScreenState extends State<LoginScreen> {
       body: Theme(
         data: Theme.of(context)
             .copyWith(primaryColor: Theme.of(context).accentColor),
-        child: Padding(
-          padding: const EdgeInsets.only(left: 16.0, right: 16, top: 54),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                "Sign In",
-                style: Theme.of(context).textTheme.headline,
-              ),
-              Form(
-                key: _key,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    SizedBox(
-                      height: 16,
-                    ),
-                    LabelTextField(
-                      labelText: "Username",
-                      onSaved: (String value) {
-                        _user.username = value;
-                      },
-                      validator: _validateUsername,
-                    ),
-                    SizedBox(
-                      height: 16,
-                    ),
-                    LabelTextField(
-                      labelText: "Password",
-                      isObscure: true,
-                      onSaved: (String value) {
-                        _user.password = value;
-                      },
-                      validator: _validatePassword,
-                    ),
-                    SizedBox(
-                      height: 16,
-                    ),
-                    Row(
-                      mainAxisSize: MainAxisSize.max,
-                      children: <Widget>[
-                        Expanded(
-                          child: RaisedButton(
-                            elevation: 0,
-                            color: Theme.of(context).accentColor,
-                            colorBrightness: Brightness.dark,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            padding: EdgeInsets.symmetric(vertical: 16.0),
-                            child: Text("Sign In"),
-                            onPressed: _onSubmit,
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 16.0, right: 16, top: 54),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Text(
+                  "Sign In",
+                  style: Theme.of(context).textTheme.headline,
                 ),
-              )
-            ],
+                Form(
+                  key: _key,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      SizedBox(
+                        height: 16,
+                      ),
+                      LabelTextField(
+                        labelText: "Username",
+                        onSaved: (String value) {
+                          _user.username = value;
+                        },
+                        validator: _validateUsername,
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      LabelTextField(
+                        labelText: "Password",
+                        isObscure: true,
+                        onSaved: (String value) {
+                          _user.password = value;
+                        },
+                        validator: _validatePassword,
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      RaisedButton(
+                        elevation: 0,
+                        color: Theme.of(context).accentColor,
+                        colorBrightness: Brightness.dark,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        padding: EdgeInsets.symmetric(vertical: 16.0),
+                        child: Text("Sign In"),
+                        onPressed: _onSubmit,
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: Center(child: Text("or")),
+                ),
+                SignInButton(
+                  Buttons.GoogleDark,
+                  onPressed: () {},
+                ),
+                SignInButton(
+                  Buttons.GitHub,
+                  onPressed: () {},
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                FlatButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 16.0),
+                  child: Text("or Create an account?"),
+                  onPressed: _onCreateAccount,
+                ),
+              ],
+            ),
           ),
         ),
       ),
@@ -111,5 +131,9 @@ class _LoginScreenState extends State<LoginScreen> {
 
       // TODO - Integrate repository login logic
     }
+  }
+
+  void _onCreateAccount() {
+    Application.router.navigateTo(context, "/signup");
   }
 }
