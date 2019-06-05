f3bf1cb3fda61afa53d267ac28815d979943051b
Udesh Kumarasinghe <mail@udeshuk.me>
2019-06-05 10:08:19 +0530

    SSO Sign in UI, sign up routing

--- labellab_mobile/lib/routing/route_handler.dart
+++ labellab_mobile/lib/routing/route_handler.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fluro/fluro.dart';
 import 'package:labellab_mobile/screen/login/login_screen.dart';
 import 'package:labellab_mobile/screen/main_screen.dart';
+import 'package:labellab_mobile/screen/sign_up/sign_up_screen.dart';
 
 var mainHandler = Handler(
     handlerFunc: (BuildContext context, Map<String, List<String>> params) {
@@ -11,4 +12,9 @@ var mainHandler = Handler(
 var loginHandler = Handler(
     handlerFunc: (BuildContext context, Map<String, List<String>> params) {
   return LoginScreen();
+});
+
+var signupHandler = Handler(
+    handlerFunc: (BuildContext context, Map<String, List<String>> params) {
+  return SignUpScreen();
 });
--- labellab_mobile/lib/routing/routes.dart
+++ labellab_mobile/lib/routing/routes.dart
@@ -4,9 +4,11 @@ import 'package:labellab_mobile/routing/route_handler.dart';
 class Routes {
   static const main = "/";
   static const login = "/login";
+  static const signup = "/signup";
 
   static void configureRouter(Router router) {
     router.define(main, handler: mainHandler);
     router.define(login, handler: loginHandler, transitionType: TransitionType.native);
+    router.define(signup, handler: signupHandler, transitionType: TransitionType.native);
   }
 }
