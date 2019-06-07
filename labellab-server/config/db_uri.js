676c3c305f081c686f1bb906884744a8ddafd1c3
Udesh Kumarasinghe <mail@udeshuk.me>
2019-06-07 11:51:58 +0530

    server: Add DB_Cluster variable

--- labellab-server/config/db_uri.js
+++ labellab-server/config/db_uri.js
@@ -3,13 +3,14 @@ require("dotenv").config()
 
 var dbHost = process.env.DB_HOST || "localhost"
 var dbName = process.env.DB_NAME
+var dbCluster = process.env.DB_CLUSTER
 var dbUser = process.env.DB_USERNAME
 var dbPass = process.env.DB_PASSWORD
 
 let mongoURI
 
 if(dbName && dbUser && dbPass){
-	mongoURI = "" + dbHost + "://"+ dbUser + ":" + dbPass + "@ds261716.mlab.com:61716/" + dbName
+	mongoURI = "" + dbHost + "://"+ dbUser + ":" + dbPass + "@" + dbCluster + "/" + dbName
 }
 else{
 	mongoURI = ""
