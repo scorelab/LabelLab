7785316f69c926e173ed2275f550d7826639c67e
Tushar Varshney <tvarshney@me.iitr.ac.in>
2019-06-07 14:25:56 +0530

    feat: get, update, get_by_id project API's

--- labellab-server/controller/user/userControls.js
+++ labellab-server/controller/user/userControls.js
@@ -49,6 +49,35 @@ exports.projectInfo = function(req, res) {
 		})
 }
 
+exports.projectInfoId = function(req, res) {
+	if (req && req.params && req.params.id) {
+		Project.find({
+			_id: req.params.id
+		})
+			.select("project_name")
+			.exec(function(err, project) {
+				if (err) {
+					return res.status(400).send({
+						success: false,
+						msg: "Unable to connect to database. Please try again.",
+						error: err
+					})
+				}
+				if (!project) {
+					return res
+						.status(400)
+						.send({ success: false, msg: "Project not found" })
+				} else {
+					return res.json({
+						success: true,
+						msg: "Project Data Found",
+						body: project
+					})
+				}
+			})
+	} else res.status(400).send({ success: false, msg: "Invalid Data" })
+}
+
 exports.initializeProject = function(req, res) {
 	if (req && req.body && req.body.project_name) {
 		Project.findOne({ project_name: req.body.project_name }).then(project => {
@@ -92,4 +121,38 @@ exports.initializeProject = function(req, res) {
 			})
 		})
 	} else res.status(400).send({ success: false, msg: "Invalid Data" })
+}
+
+exports.updateProject = function(req, res) {
+	if (req && req.params && req.params.id) {
+		var data = {
+			project_name: req.body.project_name
+		}
+		Project.findOneAndUpdate(
+			{
+				_id: req.params.id
+			},
+			data,
+			{ new: true }
+		).exec(function(err, project) {
+			if (err) {
+				return res.status(400).send({
+					success: false,
+					msg: "Unable to connect to database. Please try again."
+				})
+			}
+			if (!project) {
+				return res
+					.status(400)
+					.send({ success: false, msg: "Project not found" })
+			}
+			res.json({
+				success: true,
+				msg: "Project updated successfully!",
+				body: project
+			})
+		})
+	} else {
+		return res.status(400).send({ success: false, msg: "Invalid Params" })
+	}
 }
