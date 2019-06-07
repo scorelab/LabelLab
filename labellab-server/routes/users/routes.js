a231fdb79edeb6fe4b294b51609cb6da196864c2
Tushar Varshney <tvarshney@me.iitr.ac.in>
2019-06-07 14:25:56 +0530

    feat: get, update, get_by_id project API's

--- labellab-server/routes/users/routes.js
+++ labellab-server/routes/users/routes.js
@@ -12,6 +12,18 @@ router.get("/info", requireAuth, userControls.userInfo)
 
 // POST method
 // To initialize user project
-router.post("/initproject", requireAuth, userControls.initializeProject)
+router.post("/create/project", requireAuth, userControls.initializeProject)
+
+// GET method
+// To fetch the project of the given id
+router.get("/get/project/:id", requireAuth, userControls.projectInfoId)
+
+// GET method
+// To fetch user project
+router.get("/get/project", requireAuth, userControls.projectInfo)
+
+// PUT method
+// To update user project
+router.put("/update/project/:id", requireAuth, userControls.updateProject)
 
 module.exports = router
