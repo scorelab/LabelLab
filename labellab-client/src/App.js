a724348de7c634ec56123f9d79a2124d274586a0
Tushar Varshney <tvarshney@me.iitr.ac.in>
2019-06-11 14:01:44 +0530

    feat: created register component for registering user

--- labellab-client/src/App.js
+++ labellab-client/src/App.js
@@ -1,30 +1,16 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
-import { createStore, applyMiddleware, compose } from "redux";
-import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-import rootReducers from "./reducers/index";
-import AppIndex from "./components/index"
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import AppIndex from "./components/index";
 import "./App.css";
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.store = createStore(
-      rootReducers,
-      compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-          window.__REDUX_DEVTOOLS_EXTENSION__()
-      )
-    );
-  }
   render() {
-    const { match } = this.props
     return (
-      <Provider store={this.store}>
-        <Route path={`${match.path}`} component={AppIndex} />
-      </Provider>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={AppIndex} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
--- labellab-client/src/reducers/register.js
+++ labellab-client/src/reducers/register.js
@@ -1,15 +1,40 @@
-const intialState ={
-	isRegistering:false,
-	error:false,
-	err_field:"",
-	statusText:""
-}
+import {
+  REGISTER_FAILURE,
+  REGISTER_REQUEST,
+  REGISTER_SUCCESS
+} from "../constants/index";
+const intialState = {
+  isRegistering: false,
+  error: false,
+  err_field: "",
+  statusText: ""
+};
+const register = (state = intialState, action) => {
+  switch (action.type) {
+    case REGISTER_REQUEST:
+      return {
+        ...state,
+        isRegistering: true,
+        error: false
+      };
+    case REGISTER_SUCCESS:
+      return {
+        ...state,
+        isRegistering: false,
+        statusText: action.payload,
+        error: false
+      };
+    case REGISTER_FAILURE:
+      return {
+        ...state,
+        isRegistering: false,
+        statusText: action.payload,
+        err_field: action.other,
+        error: true
+      };
+    default:
+      return state;
+  }
+};
 
-const register = (state = intialState , action )=>{
-	switch(action.type){
-	default:
-		return state
-	}
-}
-
-export default register
+export default register;
