12d1a7fde085de66f43820b87819c4a74695b91b
Tushar Varshney <tvarshney@me.iitr.ac.in>
2019-06-13 01:32:35 +0530

    feat: user profile image upload API and component

--- labellab-server/controller/user/userControls.js
+++ labellab-server/controller/user/userControls.js
@@ -20,3 +20,48 @@ exports.userInfo = function(req, res) {
 			}
 		})
 }
+
+exports.userUploadImage = function(req, res) {
+	if (req && req.body && req.body.image && req.body.format) {
+		let data = {
+			id: req.user.id,
+			email: req.user.email,
+			img: req.body.image,
+			format: req.body.format
+		}
+		let baseImg = data.img.split(",")[1]
+		let binaryData = new Buffer(baseImg, "base64")
+		let ext = data.format.split("/")[1]
+		let updateData = { thumbnail: `${data.id}.${ext}` }
+		const url = `/public/img/${updateData.thumbnail}`
+		require("fs").writeFile(
+			`./public/img/${updateData.thumbnail}`,
+			binaryData,
+			function(err) {
+				if (err) {
+					return res
+						.status(400)
+						.send({ success: false, msg: "something went wrong" })
+				} else {
+					User.findOneAndUpdate(
+						{
+							email: data.email
+						},
+						updateData
+					).exec(function(err) {
+						if (err)
+							return res.status(400).send({
+								success: false,
+								msg: "Unable To Upload Image. Please Try Again."
+							})
+						res.json({
+							success: true,
+							body: url,
+							msg: "Image Uploaded Successfully."
+						})
+					})
+				}
+			}
+		)
+	} else res.status(400).send({ success: false, msg: "Invalid Data" })
+}
