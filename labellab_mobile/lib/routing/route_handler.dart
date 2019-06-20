3e040d496050def2b6e01efd051cc96fb1874f28
Udesh Kumarasinghe <mail@udeshuk.me>
2019-06-20 09:26:36 +0530

    Implemented classify feature UI

--- labellab_mobile/lib/routing/route_handler.dart
+++ labellab_mobile/lib/routing/route_handler.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:fluro/fluro.dart';
+import 'package:labellab_mobile/screen/classify/classify_bloc.dart';
+import 'package:labellab_mobile/screen/classify/classify_screen.dart';
 import 'package:labellab_mobile/screen/login/login_screen.dart';
 import 'package:labellab_mobile/screen/main_screen.dart';
 import 'package:labellab_mobile/screen/profile/profile_screen.dart';
 import 'package:labellab_mobile/screen/project/add_edit/add_edit_project_screen.dart';
 import 'package:labellab_mobile/screen/sign_up/sign_up_screen.dart';
+import 'package:provider/provider.dart';
 
 var mainHandler = Handler(
     handlerFunc: (BuildContext context, Map<String, List<String>> params) {
@@ -34,4 +37,13 @@ var addProjectHandler = Handler(
 var editProjectHandler = Handler(
     handlerFunc: (BuildContext context, Map<String, List<String>> params) {
   return AddEditProjectScreen(id: params['id'].first);
+});
+
+var classifyHandler = Handler(
+    handlerFunc: (BuildContext context, Map<String, List<String>> params) {
+  return Provider<ClassifyBloc>(
+    builder: (context) => ClassifyBloc(),
+    dispose: (context, bloc) => bloc.dispose(),
+    child: ClassifyScreen(params['by'].first == 'camera'),
+  );
 });
