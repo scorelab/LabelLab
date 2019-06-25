f50ed7e6ec1a039e39eadf9abf162fa060eccdd7
Udesh Kumarasinghe <mail@udeshuk.me>
2019-06-25 23:36:51 +0530

    Implemented Google SSO

---
+++ labellab_mobile/lib/data/remote/dto/google_user_request.dart
@@ -0,0 +1,26 @@
+import 'package:google_sign_in/google_sign_in.dart';
+
+class GoogleUserRequest {
+  String id;
+  String displayName;
+  String photoUrl;
+  String email;
+  String accessToken;
+
+  GoogleUserRequest(GoogleSignInAccount user, this.accessToken) {
+    this.id = user.id;
+    this.displayName = user.displayName;
+    this.photoUrl = user.photoUrl;
+    this.email = user.email;
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      "id": id,
+      "displayName": displayName,
+      "photoUrl": photoUrl,
+      "email": email,
+      "accessToken": accessToken
+    };
+  }
+}
