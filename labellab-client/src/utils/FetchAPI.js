20fb42bdf3f9a82b5cf5ced74d30472ea62faf77
Tushar Varshney <tvarshney@me.iitr.ac.in>
2019-12-26 04:27:50 +0530

    [update]: Module updated and fixed npm packages

--- labellab-client/src/utils/FetchAPI.js
+++ labellab-client/src/utils/FetchAPI.js
@@ -4,8 +4,14 @@ import { TOKEN_TYPE } from '../constants/index'
 
 const FetchApi = (method, url, params, TokenValue) => {
   if (process.env.REACT_APP_SERVER_ENVIORNMENT === 'dev') {
-    url = process.env.REACT_APP_HOST + process.env.REACT_APP_SERVER_PORT + url
+    url =
+      'http://' +
+      process.env.REACT_APP_HOST +
+      ':' +
+      process.env.REACT_APP_SERVER_PORT +
+      url
   }
+  console.log(url)
   return new Promise((resolve, reject) => {
     if (TokenValue) {
       axios({
