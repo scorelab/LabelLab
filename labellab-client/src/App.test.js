154601990e223f0475f066bb7cbca361f94ec661
Joseph Semrai <josephsemrai@gmail.com>
2019-12-26 16:53:17 -0500

    Fix app test

--- labellab-client/src/App.test.js
+++ labellab-client/src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { store } from './utils/store'
+import { Provider } from 'react-redux'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  )
   ReactDOM.unmountComponentAtNode(div)
 })
