e4887e2f87259c01d7487da985f715524595a28f
Laksh <lakshyajeet22@gmail.com>
2020-01-01 15:40:41 +0530

    Add Delete Project button to project page

--- labellab-client/src/actions/project/index.js
+++ labellab-client/src/actions/project/index.js
@@ -1,3 +1,38 @@
+import {
+  DELETE_PROJECT_FAILURE,
+  DELETE_PROJECT_REQUEST,
+  DELETE_PROJECT_SUCCESS
+} from '../../constants/project/index'
+
+import FetchApi from '../../utils/FetchAPI'
+
+export const deleteProject = (projectId, callback) => {
+  return dispatch => {
+    dispatch(request())
+    FetchApi('DELETE', '/api/v1/project/delete/' + projectId, null, true)
+      .then(res => {
+        dispatch(success())
+        callback()
+      })
+      .catch(err => {
+        if (err.response) {
+          err.response.data
+            ? dispatch(failure(err.response.data.msg))
+            : dispatch(failure(err.response.statusText, null))
+        }
+      })
+  }
+  function request() {
+    return { type: DELETE_PROJECT_REQUEST }
+  }
+  function success() {
+    return { type: DELETE_PROJECT_SUCCESS }
+  }
+  function failure(error) {
+    return { type: DELETE_PROJECT_FAILURE, payload: error }
+  }
+}
+
 export * from './fetchDetails'
 export * from './member'
 export * from './search'
