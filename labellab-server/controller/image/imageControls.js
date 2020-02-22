4ed8b72bb2190ed514b9c1e9c9897b3bc42291bb
Laksh <lakshyajeet22@gmail.com>
2020-02-22 20:20:00 +0800

    Add option to delete multiple images at once

--- labellab-server/controller/image/imageControls.js
+++ labellab-server/controller/image/imageControls.js
@@ -222,52 +222,40 @@ exports.updateLabels = function(req, res) {
 }
 
 exports.deleteImage = function(req, res) {
-  if (req && req.params && req.params.imageId) {
-    Image.findOne({
-      _id: req.params.imageId
-    }).exec(function(err, image) {
-      if (err) {
-        return res.status(400).send({
-          success: false,
-          msg: 'Unable to connect to database. Please try again.',
-          error: err
-        })
-      } else {
-        Image.findOneAndDelete({
-          _id: image
-        })
-        fs.unlinkSync(
-          path.join(__dirname, '../../', `public/uploads/${image.imageUrl}`)
-        )
-      }
-    })
-    Image.findOneAndDelete({
-      _id: req.params.imageId
-    }).exec(function(err, image) {
-      if (err) {
-        return res.status(400).send({
-          success: false,
-          msg: 'Unable to connect to database. Please try again.',
-          error: err
-        })
-      } else {
-        Project.findOneAndUpdate(
-          { _id: image.project },
-          { $pull: { image: req.params.imageId } }
-        ).exec(function(err, project) {
-          if (err) {
-            return res.status(400).send({
-              success: false,
-              msg: 'Cannot delete image',
-              error: err
-            })
-          }
-          return res.json({
-            success: true,
-            msg: 'Image deleted successfully!'
+  if (req && req.body && req.body.images) {
+    const imageList = req.body.images
+    for (var i = 0; i < imageList.length; i++) {
+      Image.findOneAndDelete({
+        _id: imageList[i]
+      }).exec(function(err, image) {
+        if (err) {
+          return res.status(400).send({
+            success: false,
+            msg: 'Unable to connect to database. Please try again.',
+            error: err
           })
-        })
-      }
+        } else {
+          fs.unlinkSync(
+            path.join(__dirname, '../../', `public/uploads/${image.imageUrl}`)
+          )
+          Project.findOneAndUpdate(
+            { _id: image.project },
+            { $pull: { image: req.params.imageId } }
+          ).exec(function(err, project) {
+            if (err) {
+              return res.status(400).send({
+                success: false,
+                msg: 'Cannot delete image',
+                error: err
+              })
+            }
+          })
+        }
+      })
+    }
+    return res.json({
+      success: true,
+      msg: 'Image(s) deleted successfully!'
     })
   } else res.status(400).send({ success: false, msg: 'Invalid Data' })
 }
