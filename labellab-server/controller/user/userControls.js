911b31851b1a0ec2b54e6e63c0917dc64f0a07bc
Laksh <lakshyajeet22@gmail.com>
2020-02-23 21:59:49 +0800

    Add option to remove profile picture

--- labellab-server/controller/user/userControls.js
+++ labellab-server/controller/user/userControls.js
@@ -1,146 +1,185 @@
+const path = require('path')
+
 let User = require('../../models/user')
 
 exports.userInfo = function(req, res) {
-	User.findOne({
-		email: req.user.email
-	})
-		.select('name email thumbnail googleId githubId username profileImage')
-		.exec(function(err, user) {
-			if (err) {
-				return res.status(400).send({
-					success: false,
-					msg: 'Unable to connect to database. Please try again.',
-					error: err
-				})
-			}
-			if (!user) {
-				return res.status(400).send({ success: false, msg: 'User not found' })
-			} else {
-				return res.json({ success: true, msg: 'User Data Found', body: user })
-			}
-		})
+  User.findOne({
+    email: req.user.email
+  })
+    .select('name email thumbnail googleId githubId username profileImage')
+    .exec(function(err, user) {
+      if (err) {
+        return res.status(400).send({
+          success: false,
+          msg: 'Unable to connect to database. Please try again.',
+          error: err
+        })
+      }
+      if (!user) {
+        return res.status(400).send({ success: false, msg: 'User not found' })
+      } else {
+        return res.json({ success: true, msg: 'User Data Found', body: user })
+      }
+    })
 }
 
 exports.editInfo = function(req, res) {
-	User.findOneAndUpdate(
-		{
-			email: req.user.email
-		},
-		req.body,
-		{ new: true }
-	).exec(function(err, user) {
-		if (err) {
-			return res.status(400).send({
-				success: false,
-				msg: 'Unable to connect to database. Please try again.',
-				error: err
-			})
-		}
-		if (!user) {
-			return res.status(400).send({ success: false, msg: 'User not updated' })
-		} else {
-			return res.json({ success: true, msg: 'User updated!', body: user })
-		}
-	})
+  User.findOneAndUpdate(
+    {
+      email: req.user.email
+    },
+    req.body,
+    { new: true }
+  ).exec(function(err, user) {
+    if (err) {
+      return res.status(400).send({
+        success: false,
+        msg: 'Unable to connect to database. Please try again.',
+        error: err
+      })
+    }
+    if (!user) {
+      return res.status(400).send({ success: false, msg: 'User not updated' })
+    } else {
+      return res.json({ success: true, msg: 'User updated!', body: user })
+    }
+  })
 }
 
 exports.searchUser = function(req, res) {
-	if (req && req.params && req.params.query) {
-		User.find(
-			{
-				email: { $regex: req.params.query, $ne: req.user.email }
-			},
-			function(err, user) {
-				if (err) return console.log(err)
-				res.json({
-					success: true,
-					body: user
-				})
-			}
-		)
-	} else {
-		return res.status(400).send({ success: false, msg: 'Invalid Params' })
-	}
+  if (req && req.params && req.params.query) {
+    User.find(
+      {
+        email: { $regex: req.params.query, $ne: req.user.email }
+      },
+      function(err, user) {
+        if (err) return console.log(err)
+        res.json({
+          success: true,
+          body: user
+        })
+      }
+    )
+  } else {
+    return res.status(400).send({ success: false, msg: 'Invalid Params' })
+  }
 }
 
 exports.countInfo = function(req, res) {
-	User.findOne({
-		_id: req.user._id
-	})
-		.select('email')
-		.populate('project')
-		.exec(function(err, user) {
-			if (err) {
-				return res.status(400).send({
-					success: false,
-					msg: 'Unable to connect to database. Please try again.',
-					error: err
-				})
-			}
-			if (!user) {
-				return res.status(400).send({ success: false, msg: 'User not found' })
-			} else {
-				let data = {
-					totalProjects: user.project.length,
-					totalImages: 0,
-					totalLabels: 0
-				}
-				user.project.map((project, index) => {
-					data.totalImages += project.image.length
-					data.totalLabels += project.labels.length
-				})
+  User.findOne({
+    _id: req.user._id
+  })
+    .select('email')
+    .populate('project')
+    .exec(function(err, user) {
+      if (err) {
+        return res.status(400).send({
+          success: false,
+          msg: 'Unable to connect to database. Please try again.',
+          error: err
+        })
+      }
+      if (!user) {
+        return res.status(400).send({ success: false, msg: 'User not found' })
+      } else {
+        let data = {
+          totalProjects: user.project.length,
+          totalImages: 0,
+          totalLabels: 0
+        }
+        user.project.map((project, index) => {
+          data.totalImages += project.image.length
+          data.totalLabels += project.labels.length
+        })
 
-				return res.json({ success: true, body: data })
-			}
-		})
+        return res.json({ success: true, body: data })
+      }
+    })
 }
 
 exports.userUploadImage = function(req, res) {
-	if (req && req.body && req.body.image && req.body.format) {
-		const { id, email } = req.user
-		const { image, format } = req.body
-		let data = {
-			id: id,
-			email: email,
-			img: image,
-			format: format
-		}
-		let baseImg = data.img.split(',')[1]
-		let binaryData = new Buffer(baseImg, 'base64')
-		let ext = data.format.split('/')[1]
-		let updateData = { profileImage: `${data.id}.${ext}` }
-		const url = `/public/img/${updateData.profileImage}`
-		require('fs').writeFile(
-			`./public/img/${updateData.profileImage}`,
-			binaryData,
-			function(err) {
-				if (err) {
-					return res
-						.status(400)
-						.send({ success: false, msg: 'something went wrong' })
-				} else {
-					User.findOneAndUpdate(
-						{
-							email: data.email
-						},
-						{
-							profileImage:
-								'http://localhost:4000/static/img/' + updateData.profileImage
-						}
-					).exec(function(err) {
-						if (err)
-							return res.status(400).send({
-								success: false,
-								msg: 'Unable To Upload Image. Please Try Again.'
-							})
-						res.json({
-							success: true,
-							body: url,
-							msg: 'Image Uploaded Successfully.'
-						})
-					})
-				}
-			}
-		)
-	} else res.status(400).send({ success: false, msg: 'Invalid Data' })
+  let isUploadingImage = false
+  let profileURL = ''
+  const { id, email } = req.user
+  let data, binaryData, updateData, url
+
+  if (req && req.body && req.body.image == null) {
+    isUploadingImage = false
+    data = {
+      email: email
+    }
+    profileURL = ''
+  } else if (req && req.body && req.body.image && req.body.format) {
+    isUploadingImage = true
+    const { image, format } = req.body
+    data = {
+      id: id,
+      email: email,
+      img: image,
+      format: format
+    }
+    let baseImg = data.img.split(',')[1]
+    binaryData = new Buffer(baseImg, 'base64')
+    let ext = data.format.split('/')[1]
+    updateData = { profileImage: `${data.id}.${ext}` }
+    url = `/public/img/${updateData.profileImage}`
+    profileURL = 'http://localhost:4000/static/img/' + updateData.profileImage
+  }
+
+  User.findOneAndUpdate(
+    {
+      email: data.email
+    },
+    {
+      profileImage: profileURL
+    }
+  ).exec(function(err, user) {
+    if (err)
+      return res.status(400).send({
+        success: false,
+        msg: 'Unable To Change Image. Please Try Again.'
+      })
+
+    if (!isUploadingImage) {
+      let profileImage = user.profileImage.replace(
+        'http://localhost:4000/static/img/',
+        ''
+      )
+      if (
+        profileImage != '' &&
+        require('fs').existsSync(
+          path.join(__dirname, '../../', `public/img/${profileImage}`)
+        )
+      ) {
+        require('fs').unlinkSync(
+          path.join(__dirname, '../../', `public/img/${profileImage}`)
+        )
+        res.json({
+          success: true,
+          body: url,
+          msg: 'Image Removed Successfully.'
+        })
+      } else {
+        return res.status(400).send({ success: false, msg: 'No saved image' })
+      }
+    } else {
+      require('fs').writeFile(
+        `./public/img/${updateData.profileImage}`,
+        binaryData,
+        function(err) {
+          if (err) {
+            return res
+              .status(400)
+              .send({ success: false, msg: 'Something went wrong' })
+          } else {
+            res.json({
+              success: true,
+              body: url,
+              msg: 'Image Uploaded Successfully.'
+            })
+          }
+        }
+      )
+    }
+  })
 }
