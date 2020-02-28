8300b1c238312a0be1d6fb4be2b784fe035249ec
the catalyst <adityabhatnagar14@gmail.com>
2020-02-28 20:39:59 +0530

    Add basic flask structure for the app

---
+++ labellab-flask/routes/users.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+
+from controllers import userscontroller
+
+usersprint = Blueprint("users", __name__)
+
+usersprint.add_url_rule(
+    "/login", view_func=userscontroller.login_user, methods=["GET"]
+)
