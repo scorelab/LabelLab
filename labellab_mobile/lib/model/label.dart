44cd46e1d942e946fd067036170e31a467650b50
Sashika Nawarathne <hi@sashika.me>
2020-03-06 13:18:19 +0530

    Fix label delete issue

--- labellab_mobile/lib/model/label.dart
+++ labellab_mobile/lib/model/label.dart
@@ -7,17 +7,20 @@ class Label {
   String id;
   String name;
   String type;
+  String mongoId;
 
-  Label({this.id, this.name, this.type});
+  Label({this.id, this.name, this.type, this.mongoId});
 
   Label.fromJson(dynamic json) {
     id = json["id"];
     name = json["name"];
     type = json["type"];
+    mongoId = json["_id"];
   }
 
   Map<String, dynamic> toMap() {
     return {
+      "_id": mongoId,
       "id": id,
       "name": name,
       "type": type,
