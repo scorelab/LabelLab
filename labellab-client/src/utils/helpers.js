aa7ce3ca2d78a93ac22d4b58a3cd22c687f0b1e3
Anjula Karunarathne <anjulakarunarathne@gmail.com>
2020-03-08 15:50:47 +0530

    Redesign login and register interfaces

--- labellab-client/src/utils/helpers.js
+++ labellab-client/src/utils/helpers.js
@@ -9,3 +9,19 @@ export const isEmail = email =>
   /^[a-zA-Z0-9._%+-]+@[A-Za-z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email)
 
 export const isMobilePhone = phone => /^(\+?91|0)?[789]\d{9}$/.test(phone)
+
+export const validateForm = ({ errors, ...rest }) => {
+  let valid = true
+
+  // validate errors being empty
+  Object.values(errors).forEach(val => {
+    val.length && (valid = false)
+  })
+
+  // validate if form is empty
+  Object.values(rest).forEach(val => {
+    !val.length && (valid = false)
+  })
+
+  return valid
+}
