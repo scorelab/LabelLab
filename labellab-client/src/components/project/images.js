307e3820038ac745befb5100fb9562a7e0cc82dd
Anjula Karunarathne <anjulakarunarathne@gmail.com>
2020-03-13 17:14:14 +0530

    Cleanup environment variable: remove colon

--- labellab-client/src/components/project/images.js
+++ labellab-client/src/components/project/images.js
@@ -285,9 +285,7 @@ const Row = ({
     <Table.Cell style={columnStyles[1]}>
       <a
         href={
-          process.env.REACT_APP_HOST +
-          process.env.REACT_APP_SERVER_PORT +
-          `/static/uploads/${image.imageUrl}?${Date.now()}`
+          `${process.env.REACT_APP_HOST}:${process.env.REACT_APP_SERVER_PORT}/static/uploads/${image.imageUrl}?${Date.now()}`
         }
       >
         {image.imageName}
