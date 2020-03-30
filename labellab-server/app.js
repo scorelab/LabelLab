bc3525cc5d347acfc7b33f3666dc49a83fe12fbb
theLYKAN <shreyambesh@gmail.com>
2020-03-31 00:21:40 +0530

    Fixed the error Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client which used to come when a request was made to a endpoint which doesn't exist

--- labellab-server/app.js
+++ labellab-server/app.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 require('dotenv').config()
 
 var createError = require('http-errors')
@@ -17,11 +18,11 @@ const { handleError } = require('./utils/error')
 var app = express()
 
 if (process.env.GOOGLE_CLIENT_ID) {
-	const passportGoogle = require('./config/googlePassport')
+    const passportGoogle = require('./config/googlePassport')
 }
 
 if (process.env.GITHUB_CLIENT_ID) {
-	const passportGithub = require('./config/githubPassport')
+    const passportGithub = require('./config/githubPassport')
 }
 
 app.use(logger('dev'))
@@ -40,31 +41,31 @@ require('./config/passport').passport
 app.use(passport.initialize())
 app.use(passport.session())
 app.use('/', indexRouter)
-// Connect to MongoDB
+    // Connect to MongoDB
 mongoose
-	.connect(config.mongoURI, {
-		promiseLibrary: require('bluebird'),
+    .connect(config.mongoURI, {
+        promiseLibrary: require('bluebird'),
         useNewUrlParser: true,
         useFindAndModify: false,
         useCreateIndex: true
-	})
-	.then(() => console.log('MongoDB connected successfully!'))
-	.catch(err => console.error(err))
+    })
+    .then(() => console.log('MongoDB connected successfully!'))
+    .catch(err => console.error(err))
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-	next(createError(404))
+app.use(function(req, res, next) {
+    next(createError(404))
 })
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
 
-	//Calling handle error funtion imported from utils error.js
-	handleError(err, res);
+    //Calling handle error funtion imported from utils error.js
+    handleError(err, res);
 
-	// render the error page
-	res.status(err.status || 500)
-	res.render('error.ejs')
+    // render the error page
+    //res.status(err.status || 500)
+    //res.render('error.ejs')
 })
 
 module.exports = app
