bb790c6ea7337399580f19301e1661aa6ed54b2c
Tushar <tvarshney@me.iitr.ac.in>
2020-05-20 20:28:19 +0530

    [fix] Fixed typo error

--- labellab-client/src/utils/FetchAPI.js
+++ labellab-client/src/utils/FetchAPI.js
@@ -3,11 +3,10 @@ import { getToken } from './token'
 import { TOKEN_TYPE } from '../constants/index'
 
 const FetchApi = (method, url, params, TokenType) => {
-  if (process.env.REACT_APP_SERVER_ENVIORNMENT === 'dev') {
+  if (process.env.REACT_APP_SERVER_ENVIRONMENT === 'dev') {
     url =
-    `${process.env.REACT_APP_HOST}:${process.env.REACT_APP_SERVER_PORT}${url}`
+      `${process.env.REACT_APP_HOST}:${process.env.REACT_APP_SERVER_PORT}${url}`
   }
-  console.log(url)
   return new Promise((resolve, reject) => {
     if (TokenType) {
       axios({
