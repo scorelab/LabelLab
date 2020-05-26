892c10b28e7ed19638bb5050205288ab21e77cd0
Laksh <lakshyajeet22@gmail.com>
2020-05-26 21:42:19 +0800

    Add preprocessing steps sub-module

--- labellab-client/src/actions/model.js
+++ labellab-client/src/actions/model.js
@@ -1,4 +1,10 @@
-import { ADD_LABEL, REMOVE_LABEL } from '../constants/index'
+import {
+  ADD_LABEL,
+  REMOVE_LABEL,
+  ADD_PREPROCESSING_STEP,
+  REMOVE_PREPROCESSING_STEP,
+  SET_TRAIN_TEST_SPLIT
+} from '../constants/index'
 
 export const addLabel = (labels, labelToAdd) => {
   if (!labels.includes(labelToAdd)) {
@@ -17,5 +23,35 @@ export const removeLabel = (labels, labelToRemove) => {
   return {
     type: REMOVE_LABEL,
     payload: labels
+  }
+}
+
+export const addPreprocessingStep = (steps, stepToAdd) => {
+  if (steps.filter(step => step.name === stepToAdd.name).length <= 0) {
+    steps.push(stepToAdd)
+  }
+
+  return {
+    type: ADD_PREPROCESSING_STEP,
+    payload: steps
+  }
+}
+
+export const removePreprocessingStep = (steps, stepToRemove) => {
+  steps = steps.filter(step => step.name !== stepToRemove)
+
+  return {
+    type: REMOVE_PREPROCESSING_STEP,
+    payload: steps
+  }
+}
+
+export const setTrainTestSplit = (field, value) => {
+  const payload = {}
+  payload[field] = value
+
+  return {
+    type: SET_TRAIN_TEST_SPLIT,
+    payload
   }
 }
