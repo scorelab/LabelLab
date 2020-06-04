d4261c89a4bd888f4d63a9e2173244811b316bda
Sashika Nawarathne <hi@sashika.me>
2020-06-04 14:17:41 +0530

    Implement label resize and update

--- labellab_mobile/lib/screen/project/common/label_selection_list.dart
+++ labellab_mobile/lib/screen/project/common/label_selection_list.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:labellab_mobile/model/label_selection.dart';
 
+typedef void OnClickSelection(LabelSelection selection);
 typedef void OnDeleteSelection(LabelSelection selection);
 
 class LabelSelectionList extends StatelessWidget {
   final List<LabelSelection> selections;
   final bool isEditable;
+  final OnClickSelection onTap;
   final OnDeleteSelection onDeleted;
 
-  LabelSelectionList(this.selections, this.isEditable, {this.onDeleted});
+  LabelSelectionList(this.selections, this.isEditable,
+      {this.onTap, this.onDeleted});
 
   @override
   Widget build(BuildContext context) {
@@ -22,18 +25,26 @@ class LabelSelectionList extends StatelessWidget {
           children: selections.map((selection) {
             return Padding(
               padding: const EdgeInsets.only(left: 8.0),
-              child: Chip(
-                label: Text(selection.label.name),
-                backgroundColor: selection.color,
-                deleteIcon: isEditable ? Icon(Icons.cancel) : null,
-                onDeleted: isEditable
-                    ? () {
-                        if (onDeleted != null) {
-                          onDeleted(selection);
+              child: InkWell(
+                  child: Chip(
+                    label: Text(selection.label.name),
+                    backgroundColor: selection.color,
+                    deleteIcon: isEditable ? Icon(Icons.cancel) : null,
+                    onDeleted: isEditable
+                        ? () {
+                            if (onDeleted != null) {
+                              onDeleted(selection);
+                            }
+                          }
+                        : null,
+                  ),
+                  onTap: isEditable
+                      ? () {
+                          if (onTap != null) {
+                            onTap(selection);
+                          }
                         }
-                      }
-                    : null,
-              ),
+                      : null),
             );
           }).toList(),
         ),
