1e21c680ce9659f9c0238aa14b0ff07702dd99ca
carpecodeum <adityabhatnagar14@gmail.com>
2020-06-19 18:56:08 +0530

    Add label-routes and controllers

--- labellab-flask/api/models/Label.py
+++ labellab-flask/api/models/Label.py
@@ -15,7 +15,7 @@ class Label(db.Model):
     __tablename__ = "label"
 
     id = db.Column(db.Integer, primary_key=True)
-    labelname = db.Column(db.String(128), nullable=False,)
+    label_name = db.Column(db.String(128), nullable=False,)
     label_type = db.Column(db.String(80), nullable=False,)
     count = db.Column(db.Integer,
                    default = 0)
@@ -28,22 +28,16 @@ class Label(db.Model):
                                  cascade="all, delete-orphan")
     models = db.relationship("MLModel", secondary=model_has_label, backref=db.backref("labels", lazy="dynamic"))
     
-    def __init__(self, labelname, label_type, count, project_id):
+    def __init__(self, label_name, label_type, project_id):
         """
         Initializes the label instance
         """
-        self.labelname = labelname
+        self.label_name = label_name
         self.label_type = label_type
-        self.count = count
         self.project_id = project_id
 
     def __repr__(self):
         """
         Returns the object reprensentation
         """
-        return "<Label %r>" % self.labelname
-    
-    @classmethod
-    def find_by_id_in_project(cls, _id, project_id):
-        result = cls.query.filter_by(id=_id, project_id=project_id).first()
-        return result
+        return "<Label %r>" % self.label_name
