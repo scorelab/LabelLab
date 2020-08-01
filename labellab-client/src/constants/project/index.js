145efffbaf3291b3834891a799cc2e3ea7628854
carpecodeum <adityabhatnagar14@gmail.com>
2020-08-01 16:12:34 +0530

    Remove import of teams files in actions and reducers

--- labellab-client/src/constants/project/index.js
+++ labellab-client/src/constants/project/index.js
@@ -3,7 +3,6 @@ export * from './fetchDetails'
 export * from './search'
 export * from './projectDetails'
 export * from './pathTracking'
-export * from './teams'
 export const DELETE_PROJECT_REQUEST = 'DELETE_PROJECT_REQUEST',
   DELETE_PROJECT_FAILURE = 'DELETE_PROJECT_FAILURE',
   DELETE_PROJECT_SUCCESS = 'DELETE_PROJECT_SUCCESS'
