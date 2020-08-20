e0d01d8359ae9bf46e88308e1ea34225bf81e89c
Sashika Nawarathne <hi@sashika.me>
2020-08-21 00:46:54 +0530

    Implement metadata fetch and view

--- labellab_mobile/lib/screen/project/view_image_path/project_image_path_screen.dart
+++ labellab_mobile/lib/screen/project/view_image_path/project_image_path_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:labellab_mobile/model/location.dart';
 import 'package:labellab_mobile/screen/project/view_image_path/project_image_path_bloc.dart';
 import 'package:labellab_mobile/screen/project/view_image_path/project_image_path_state.dart';
 import 'package:provider/provider.dart';
@@ -13,38 +12,79 @@ class ProjectImagePathScreen extends StatelessWidget {
       initialData: ProjectImagePathState.loading(),
       builder: (BuildContext context,
           AsyncSnapshot<ProjectImagePathState> snapshot) {
-        List<Location> locations = snapshot.data.locations;
-        return Scaffold(
-          appBar: AppBar(
-            title: Text("Image Path"),
-            elevation: 0,
-          ),
-          body: locations != null
-              ? Container(
-                  margin: EdgeInsets.symmetric(vertical: 4.0),
-                  child: GoogleMap(
-                    mapType: MapType.hybrid,
-                    initialCameraPosition: CameraPosition(
-                      target: LatLng(
-                          locations.first.latitude, locations.first.longitude),
-                      zoom: 12,
-                    ),
-                    onMapCreated: (GoogleMapController controller) {},
-                    markers: locations
-                        .map(
-                          (location) => Marker(
-                            markerId: MarkerId(location.name),
-                            position:
-                                LatLng(location.latitude, location.longitude),
-                            infoWindow: InfoWindow(title: location.name),
-                          ),
-                        )
-                        .toSet(),
+        if (snapshot.hasData) {
+          ProjectImagePathState _state = snapshot.data;
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("Object Path"),
+              elevation: 0,
+            ),
+            body: _state.isLoading
+                ? LinearProgressIndicator()
+                : Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      _state.labels != null
+                          ? Wrap(
+                              spacing: 8,
+                              children: _state.labels.map((label) {
+                                return InkWell(
+                                  child: Chip(
+                                    label: Text(label.name),
+                                  ),
+                                  onTap: () => _updateMap(context, label.id),
+                                );
+                              }).toList(),
+                            )
+                          : Container(),
+                      Expanded(
+                        child: _state.locations != null
+                            ? Container(
+                                margin: EdgeInsets.symmetric(vertical: 4.0),
+                                child: _state.locations != null
+                                    ? GoogleMap(
+                                        mapType: MapType.hybrid,
+                                        initialCameraPosition: CameraPosition(
+                                          target: LatLng(
+                                              _state.locations.first.latitude,
+                                              _state.locations.first.longitude),
+                                          zoom: 12,
+                                        ),
+                                        onMapCreated:
+                                            (GoogleMapController controller) {},
+                                        markers: _state.locations
+                                            .map(
+                                              (location) => Marker(
+                                                markerId:
+                                                    MarkerId(location.name),
+                                                position: LatLng(
+                                                    location.latitude,
+                                                    location.longitude),
+                                                infoWindow: InfoWindow(
+                                                    title: location.name),
+                                              ),
+                                            )
+                                            .toSet(),
+                                      )
+                                    : Text(
+                                        "Need more data to generate the path"),
+                              )
+                            : Center(
+                                child: Container(
+                                  child: Text("Please select a label first"),
+                                ),
+                              ),
+                      ),
+                    ],
                   ),
-                )
-              : Container(),
-        );
+          );
+        }
+        return Container();
       },
     );
+  }
+
+  void _updateMap(BuildContext context, String labelId) {
+    Provider.of<ProjectImagePathBloc>(context).selectLabel(labelId);
   }
 }
