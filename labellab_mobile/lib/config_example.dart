a8cee4ef1ef7899e984c61c07a13fc825643f9e9
PaRaDoX50 <suryansh.stomar.min19@itbhu.ac.in>
2021-02-04 01:01:16 +0530

    fixed BottomOverFlowed error of NavBarItem

--- labellab_mobile/lib/config_example.dart
+++
@@ -1,5 +0,0 @@
-/// This is the configuration file for LabelLab Mobile app.
-///
-/// Update following values with respective values and save the file as "config.dart" in the current directory.
-
-const String API_BASE_URL = "";
