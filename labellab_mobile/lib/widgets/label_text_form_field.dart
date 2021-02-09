734b68b4de53056e2bacccaf28b634826c02842b
Amlan <amlannandy5@gmail.com>
2021-02-09 17:49:31 +0530

    add show password option in text field

--- labellab_mobile/lib/widgets/label_text_form_field.dart
+++ labellab_mobile/lib/widgets/label_text_form_field.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class LabelTextFormField extends StatelessWidget {
+class LabelTextFormField extends StatefulWidget {
   final Key key;
   final String hintText;
   final String labelText;
@@ -24,25 +24,42 @@ class LabelTextFormField extends StatelessWidget {
   });
 
   @override
+  _LabelTextFormFieldState createState() => _LabelTextFormFieldState();
+}
+
+class _LabelTextFormFieldState extends State<LabelTextFormField> {
+  bool _textShown = true;
+
+  @override
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(8),
       child: TextFormField(
-        key: key,
+        key: widget.key,
         decoration: new InputDecoration(
-          hintText: hintText,
-          labelText: labelText,
-          errorText: errorText,
+          hintText: widget.hintText,
+          labelText: widget.labelText,
+          errorText: widget.errorText,
           labelStyle: TextStyle(color: Colors.black45),
           filled: true,
           fillColor: Colors.black12,
           border: InputBorder.none,
+          suffixIcon: widget.isObscure
+              ? GestureDetector(
+                  onTap: () => setState(() => _textShown = !_textShown),
+                  child: Icon(
+                    _textShown ? Icons.visibility : Icons.visibility_off,
+                    size: 20,
+                    color: Colors.black45,
+                  ),
+                )
+              : null,
         ),
-        textCapitalization: textCapitalization,
-        keyboardType: keyboardType,
-        obscureText: isObscure,
-        validator: validator,
-        onSaved: onSaved,
+        textCapitalization: widget.textCapitalization,
+        keyboardType: widget.keyboardType,
+        obscureText: widget.isObscure ? _textShown : widget.isObscure,
+        validator: widget.validator,
+        onSaved: widget.onSaved,
       ),
     );
   }
