d172c6126a357e9864df1de251b8cbf36e42345c
Amlan <amlannandy5@gmail.com>
2021-06-03 20:02:45 +0530

    back: added username to log model

--- labellab-flask/api/models/Log.py
+++ labellab-flask/api/models/Log.py
@@ -9,13 +9,15 @@ class Log(db.Model):
   entity_type = db.Column(db.String(10))
   entity_id = db.Column(db.Integer)
   user_id = db.Column(db.Integer, nullable=False)
+  username = db.Column(db.String(20), nullable=False)
   project_id = db.Column(db.Integer, nullable=False)
   timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
-  def __init__(self, message, category, entity_type, entity_id, user_id, project_id):
+  def __init__(self, message, category, entity_type, entity_id, user_id, project_id, username):
     self.message = message
     self.category = category
     self.entity_type = entity_type
     self.entity_id = entity_id
     self.user_id = user_id
     self.project_id = project_id
+    self.username = username
