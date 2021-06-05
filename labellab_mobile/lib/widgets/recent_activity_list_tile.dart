475935cc8c60d98cc1fb403b2c51ae533b4f218c
Amlan <amlannandy5@gmail.com>
2021-06-05 16:41:57 +0530

    mobile: added color coding and loading indicator

--- labellab_mobile/lib/widgets/recent_activity_list_tile.dart
+++ labellab_mobile/lib/widgets/recent_activity_list_tile.dart
@@ -3,22 +3,27 @@ import 'package:labellab_mobile/model/log.dart';
 
 class RecentActivityListTile extends StatelessWidget {
   final Log log;
+  final bool isCustomized;
 
-  RecentActivityListTile(this.log);
+  RecentActivityListTile(this.log, {this.isCustomized = false});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(
-        vertical: 7.5,
+      padding: EdgeInsets.symmetric(
+        vertical: isCustomized ? 10 : 7.5,
         horizontal: 10,
       ),
-      margin: const EdgeInsets.only(bottom: 5),
+      margin: EdgeInsets.only(bottom: isCustomized ? 7.5 : 5),
       decoration: BoxDecoration(
-        color: Color(0xff01A8A0).withOpacity(0.2),
+        color: isCustomized
+            ? _getBackgroundColor(log.category!)
+            : Color(0xff01A8A0).withOpacity(0.2),
         border: Border.all(
           width: 1,
-          color: Color(0xff01A8A0),
+          color: isCustomized
+              ? _getTextOrBorderColor(log.category!)
+              : Color(0xff01A8A0),
         ),
         borderRadius: BorderRadius.circular(5),
       ),
@@ -30,7 +35,9 @@ class RecentActivityListTile extends StatelessWidget {
             style: TextStyle(
               fontSize: 14,
               fontWeight: FontWeight.bold,
-              color: Color(0xff01A8A0),
+              color: isCustomized
+                  ? _getTextOrBorderColor(log.category!)
+                  : Color(0xff01A8A0),
             ),
           ),
           SizedBox(height: 5),
@@ -40,14 +47,18 @@ class RecentActivityListTile extends StatelessWidget {
                 log.timestamp!,
                 style: TextStyle(
                   fontSize: 12,
-                  color: Color(0xff01A8A0),
+                  color: isCustomized
+                      ? _getTextOrBorderColor(log.category!)
+                      : Color(0xff01A8A0),
                 ),
               ),
               Text(
                 "  (by ${log.username!})",
                 style: TextStyle(
                   fontSize: 12,
-                  color: Color(0xff01A8A0),
+                  color: isCustomized
+                      ? _getTextOrBorderColor(log.category!)
+                      : Color(0xff01A8A0),
                 ),
               ),
             ],
@@ -55,5 +66,35 @@ class RecentActivityListTile extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  Color _getBackgroundColor(String category) {
+    switch (category) {
+      case 'general':
+        return Color(0xff3A35C4).withOpacity(0.3);
+      case 'images':
+        return Color(0xff0C7800).withOpacity(0.3);
+      case 'labels':
+        return Color(0xff980000).withOpacity(0.3);
+      case 'models':
+        return Color(0xffCBBD00).withOpacity(0.3);
+      default:
+        return Colors.black.withOpacity(0.2);
+    }
+  }
+
+  Color _getTextOrBorderColor(String category) {
+    switch (category) {
+      case 'general':
+        return Color(0xff3A35C4);
+      case 'images':
+        return Color(0xff0C7800);
+      case 'labels':
+        return Color(0xff980000);
+      case 'models':
+        return Color(0xffCBBD00);
+      default:
+        return Colors.black;
+    }
   }
 }
