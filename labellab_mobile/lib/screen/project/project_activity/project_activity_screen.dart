75e5331df3fb67ef97f1768db0c2b083cf4322de
Amlan <amlannandy5@gmail.com>
2021-06-10 13:02:37 +0530

    added empty placeholder in logs screen

--- labellab_mobile/lib/screen/project/project_activity/project_activity_screen.dart
+++ labellab_mobile/lib/screen/project/project_activity/project_activity_screen.dart
@@ -3,6 +3,7 @@ import 'package:labellab_mobile/screen/project/detail/project_detail_bloc.dart';
 import 'package:labellab_mobile/screen/project/project_activity/local_widgets/filter_bottom_sheet.dart';
 import 'package:labellab_mobile/screen/project/project_activity/project_activity_bloc.dart';
 import 'package:labellab_mobile/screen/project/project_activity/project_activity_state.dart';
+import 'package:labellab_mobile/widgets/empty_placeholder.dart';
 import 'package:labellab_mobile/widgets/recent_activity_list_tile.dart';
 import 'package:provider/provider.dart';
 
@@ -42,16 +43,18 @@ class ProjectActivityScreen extends StatelessWidget {
             },
             child: _state!.isLoading
                 ? _loadingIndicator(context)
-                : Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ListView.builder(
-                      itemBuilder: (ctx, index) => RecentActivityListTile(
-                        _state.logs![index],
-                        isCustomized: true,
+                : _state.logs!.isEmpty
+                    ? EmptyPlaceholder(description: 'No logs available')
+                    : Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ListView.builder(
+                          itemBuilder: (ctx, index) => RecentActivityListTile(
+                            _state.logs![index],
+                            isCustomized: true,
+                          ),
+                          itemCount: _state.logs!.length,
+                        ),
                       ),
-                      itemCount: _state.logs!.length,
-                    ),
-                  ),
           );
         },
       ),
