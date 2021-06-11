d50c21a6c676f03d4d680d1f98801b3bb6ab4f58
Amlan <amlannandy5@gmail.com>
2021-06-11 13:22:11 +0530

    backend: created accesed decorators

---
+++ labellab-flask/api/middleware/project_member_access.py
@@ -0,0 +1,38 @@
+from functools import wraps
+from flask import jsonify, make_response
+from flask_jwt_extended import get_jwt_identity
+
+from api.helpers.project import find_by_project_id
+from api.helpers.user import get_user_roles
+
+def project_member_only(fun):
+  @wraps(fun)
+  def wrap(*args, **kwargs):
+
+    # Get current user
+    user_id = get_jwt_identity()
+
+    # Get project and admin_id
+    project_id = kwargs.get('project_id')
+    project = find_by_project_id(project_id)
+
+    if not project:
+      response = {
+        'success': False,
+        'msg': 'Project does not exist',
+      }
+      return make_response(jsonify(response)), 404
+
+    user_roles = get_user_roles(user_id, project_id)
+
+    # If current user has no roles in the project, then unauthorized
+    if len(user_roles) == 0:
+      response = {
+        'success': False,
+        'msg': 'Only a project member can access this route',
+      }
+      return make_response(jsonify(response)), 401
+
+    # Else continue
+    return fun(*args, **kwargs)
+  return wrap
