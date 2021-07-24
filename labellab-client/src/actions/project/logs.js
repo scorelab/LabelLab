04098b5190ea01e7a0236b74610b14e604009358
Amlan <amlannandy5@gmail.com>
2021-07-24 13:54:37 +0530

    frontend: implemented category specific log filtering

--- labellab-client/src/actions/project/logs.js
+++ labellab-client/src/actions/project/logs.js
@@ -1,7 +1,10 @@
 import {
   FETCH_PROJECT_LOGS_REQUEST,
   FETCH_PROJECT_LOGS_SUCCESS,
-  FETCH_PROJECT_LOGS_FAILURE
+  FETCH_PROJECT_LOGS_FAILURE,
+  FETCH_CATEGORY_SPECIFIC_LOGS_REQUEST,
+  FETCH_CATEGORY_SPECIFIC_LOGS_SUCCESS,
+  FETCH_CATEGORY_SPECIFIC_LOGS_FAILURE
 } from '../../constants/index'
 import FetchApi from '../../utils/FetchAPI'
 
@@ -26,5 +29,29 @@ export const fetchProjectLogs = projectId => {
   }
   function failure(error) {
     return { type: FETCH_PROJECT_LOGS_FAILURE, payload: error }
+  }
+}
+
+export const fetchCategorySpecificLogs = (projectId, category) => {
+  return dispatch => {
+    dispatch(request())
+    FetchApi.get(`/api/v1/logs/${projectId}/category/${category}`)
+      .then(res => dispatch(success(res.data.data)))
+      .catch(err => {
+        if (err.response) {
+          err.response.data
+            ? dispatch(failure(err.response.data.msg))
+            : dispatch(failure(err.response.statusText, null))
+        }
+      })
+  }
+  function request() {
+    return { type: FETCH_CATEGORY_SPECIFIC_LOGS_REQUEST }
+  }
+  function success(data) {
+    return { type: FETCH_CATEGORY_SPECIFIC_LOGS_SUCCESS, payload: data }
+  }
+  function failure(error) {
+    return { type: FETCH_CATEGORY_SPECIFIC_LOGS_FAILURE, payload: error }
   }
 }
--- labellab-client/src/components/project/projectActivity.js
+++ labellab-client/src/components/project/projectActivity.js
@@ -1,9 +1,58 @@
 import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-import { Table, Header, Dimmer, Loader, Message, Icon } from 'semantic-ui-react'
+import {
+  Table,
+  Header,
+  Dimmer,
+  Loader,
+  Message,
+  Icon,
+  Dropdown,
+  Grid,
+  Divider
+} from 'semantic-ui-react'
+
+import {
+  fetchProjectLogs,
+  fetchCategorySpecificLogs
+} from '../../actions/index'
+
+const categoryOptions = [
+  { key: 1, value: 'all', text: 'all' },
+  { key: 2, value: 'general', text: 'general' },
+  { key: 3, value: 'images', text: 'images' },
+  { key: 4, value: 'labels', text: 'labels' },
+  {
+    key: 5,
+    value: 'image labelling',
+    text: 'image labelling'
+  },
+  { key: 6, value: 'models', text: 'models' }
+]
 
 class ProjectActivity extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      category: 'all'
+    }
+  }
+
+  filterOnCategory = (e, { value }) => {
+    const {
+      projectId,
+      fetchProjectLogs,
+      fetchCategorySpecificLogs
+    } = this.props
+    this.setState({ category: value })
+    if (value === 'all') {
+      fetchProjectLogs(projectId)
+    } else {
+      fetchCategorySpecificLogs(projectId, value)
+    }
+  }
+
   render() {
     const {
       logs,
@@ -26,6 +75,21 @@ class ProjectActivity extends Component {
             </Message.Content>
           </Message>
         ) : null}
+        <Header as="h4" subheader>
+          Category
+          <Header.Subheader as="h4" className="margin-v">
+            <Dropdown
+              name="category"
+              placeholder="Category"
+              selection
+              value={this.state.category}
+              options={categoryOptions}
+              onChange={this.filterOnCategory}
+            />
+          </Header.Subheader>
+        </Header>
+        <Divider />
+
         <Table color="black">
           <Table.Header>
             <Table.Row>
@@ -71,12 +135,20 @@ ProjectActivity.propTypes = {
 const mapStateToProps = state => {
   return {
     logs: state.logs.logs,
-    logsActions: state.logs.logsActions
+    logsActions: state.logs.logsActions,
+    projectId: state.projects.currentProject.projectId
   }
 }
 
 const mapDispatchToProps = dispatch => {
-  return {}
+  return {
+    fetchProjectLogs: projectId => {
+      return dispatch(fetchProjectLogs(projectId))
+    },
+    fetchCategorySpecificLogs: (projectId, category) => {
+      return dispatch(fetchCategorySpecificLogs(projectId, category))
+    }
+  }
 }
 
 export default connect(
