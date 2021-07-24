0a26d3fe266ddb6ab84228978061be6cbe905208
Amlan <amlannandy5@gmail.com>
2021-07-24 11:44:06 +0530

    frontend: implemented project activity UI

--- labellab-client/src/components/project/projectActivity.js
+++ labellab-client/src/components/project/projectActivity.js
@@ -1,13 +1,85 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { Table, Header, Dimmer, Loader, Message, Icon } from 'semantic-ui-react'
 
 class ProjectActivity extends Component {
   render() {
+    const {
+      logs,
+      logsActions: { isFetching, errors }
+    } = this.props
+
     return (
-      <div>
-        <h1>Project Activity</h1>
-      </div>
+      <Fragment>
+        {isFetching ? (
+          <Dimmer active>
+            <Loader indeterminate>Fetching project activity...</Loader>
+          </Dimmer>
+        ) : null}
+        {errors ? (
+          <Message icon>
+            <Icon name="warning" />
+            <Message.Content>
+              <Message.Header>Error</Message.Header>
+              {errors}
+            </Message.Content>
+          </Message>
+        ) : null}
+        <Table color="black">
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Recent Activity </Table.HeaderCell>
+              <Table.HeaderCell>Category</Table.HeaderCell>
+              <Table.HeaderCell>Member</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {logs
+              ? logs.map(log => (
+                  <Table.Row
+                    negative={log.category === 'models'}
+                    positive={log.category === 'images'}
+                    warning={log.category === 'labels'}
+                    active={log.category === 'general'}
+                    key={log.id}
+                  >
+                    <Table.Cell width={12}>
+                      <Header as="h4" subheader>
+                        {log.message}
+                        <Header.Subheader as="h6">
+                          {log.timestamp}
+                        </Header.Subheader>
+                      </Header>
+                    </Table.Cell>
+                    <Table.Cell width={4}>{log.category}</Table.Cell>
+                    <Table.Cell width={4}>{log.username}</Table.Cell>
+                  </Table.Row>
+                ))
+              : null}
+          </Table.Body>
+        </Table>
+      </Fragment>
     )
   }
 }
 
-export default ProjectActivity
+ProjectActivity.propTypes = {
+  logs: PropTypes.array
+}
+
+const mapStateToProps = state => {
+  return {
+    logs: state.logs.logs,
+    logsActions: state.logs.logsActions
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {}
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ProjectActivity)
