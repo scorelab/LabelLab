4e563b01d5f2ac2eccb501bcdcb7e4f85fff8cd2
M-A-D-A-R-A <andoriyanishant@gmail.com>
2022-06-01 16:28:13 +0530

    other end points Initializtation

--- labellab-flask/api/controllers/issuecontroller.py
+++ labellab-flask/api/controllers/issuecontroller.py
@@ -8,23 +8,40 @@ from flask_jwt_extended import (
 from api.config import config
 from api.models.Issue import Issue
 
+class CreateIssues(MethodView):
+    """
+    This class-based view handles fetching all issues within a project
+    Url --> /api/v1/issue/create/<int:project_id>
+    """
+    @jwt_required
+    def post(self, project_id):
+        try:
+                        
+            response = {
+                "success": True,
+                "msg": "Issue Created",
+                "body": "New Issue Create for this project"
+            }
+            return make_response(jsonify(response)), 200
+        
+        except Exception:
+            response = {
+                "success":False,
+                "msg": "Something went wrong!"
+                }
+            # Return a server error using the HTTP Error Code 500 (Internal
+            # Server Error)
+            return make_response(jsonify(response)), 500
 
 class GetAllIssues(MethodView):
     """
     This class-based view handles fetching all issues within a project
-    Url --> /api/v1/issue/<int:project_id>
+    Url --> /api/v1/issue/get/<int:project_id>
     """
     @jwt_required
     def get(self, project_id):
         try:
-            if not project_id:
-                response = {
-                    "success":False,
-                    "msg": "Project id not provided"
-                    }
-                return make_response(jsonify(response)), 400
-            
-            # TODO:passing the issue body to the response            
+                        
             response = {
                 "success": True,
                 "msg": "Issues found",
@@ -41,7 +58,128 @@ class GetAllIssues(MethodView):
             # Server Error)
             return make_response(jsonify(response)), 500
 
+class IssueInfo(MethodView):
+    """
+    This methods GET,DELETE and PUT the info of a particular Issue.
+    Url --> /api/v1/issue/issue_info/<int:project_id>/<int:issue_id>
+    """
+    @jwt_required
+    def get(self,project_id, issue_id):
+        try:
+                        
+            response = {
+                "success": True,
+                "msg": "Issues found",
+                "body": "Get Issue by ID"
+            }
+            return make_response(jsonify(response)), 200
+        
+        except Exception:
+            response = {
+                "success":False,
+                "msg": "Something went wrong!"
+                }
+            # Return a server error using the HTTP Error Code 500 (Internal
+            # Server Error)
+            return make_response(jsonify(response)), 500
+    
+    @jwt_required
+    def put(self,project_id, issue_id):
+        try:
+                        
+            response = {
+                "success": True,
+                "msg": "Issues updated",
+                "body": "Issue Details upadted"
+            }
+            return make_response(jsonify(response)), 200
+        
+        except Exception:
+            response = {
+                "success":False,
+                "msg": "Something went wrong!"
+                }
+            # Return a server error using the HTTP Error Code 500 (Internal
+            # Server Error)
+            return make_response(jsonify(response)), 500
+    
+    @jwt_required
+    def delete(self,project_id, issue_id):
+        try:
+                        
+            response = {
+                "success": True,
+                "msg": "Issues Deleted",
+                "body": "Issue Details deleted"
+            }
+            return make_response(jsonify(response)), 200
+        
+        except Exception:
+            response = {
+                "success":False,
+                "msg": "Something went wrong!"
+                }
+            # Return a server error using the HTTP Error Code 500 (Internal
+            # Server Error)
+            return make_response(jsonify(response)), 500
+
+class FetchCategoryIssuesView(MethodView):
+
+    """
+    This route fetches all project issue of a particular category
+     URL:- /api/v1/issue/<int:project_id>/category/<string:category>
+    """
+    @jwt_required
+    def get(self,project_id, category):
+        try:
+                        
+            response = {
+                "success": True,
+                "msg": "Issues found",
+                "body": "Get Issue by Category"
+            }
+            return make_response(jsonify(response)), 200
+        
+        except Exception:
+            response = {
+                "success":False,
+                "msg": "Something went wrong!"
+                }
+            # Return a server error using the HTTP Error Code 500 (Internal
+            # Server Error)
+            return make_response(jsonify(response)), 500
+
+class FetchTeamIssuesView(MethodView):
+
+    """
+    This route fetches all project issue of a particular category
+     URL:- /api/v1/issue/<int:project_id>/team/<string:team>
+    """
+    @jwt_required
+    def get(self,project_id, team):
+        try:
+                        
+            response = {
+                "success": True,
+                "msg": "Issues found",
+                "body": "Get Issue by team"
+            }
+            return make_response(jsonify(response)), 200
+        
+        except Exception:
+            response = {
+                "success":False,
+                "msg": "Something went wrong!"
+                }
+            # Return a server error using the HTTP Error Code 500 (Internal
+            # Server Error)
+            return make_response(jsonify(response)), 500
+
 
 issueController = {
-    "get_all_issues": GetAllIssues.as_view("get_all_issues")
+    "createissue": CreateIssues.as_view("createissue"),
+    "get_all_issues": GetAllIssues.as_view("get_all_issues"),
+    "issue": IssueInfo.as_view("issue"),
+    "fetch_category_issue":FetchCategoryIssuesView.as_view("fetch_category_issue"),
+    "fetch_team_issue": FetchTeamIssuesView.as_view("fetch_team_issue")
 }
--- labellab-flask/api/routes/issue.py
+++ labellab-flask/api/routes/issue.py
@@ -5,7 +5,31 @@ from api.controllers import issuecontroller
 issuesprint = Blueprint("issues", __name__)
 
 issuesprint.add_url_rule(
-    "/issue/<int:project_id>",
+    "/issue/create/<int:project_id>",
+    view_func=issuecontroller.issueController["createissue"], 
+    methods=["POST"]
+)
+
+issuesprint.add_url_rule(
+    "/issue/get/<int:project_id>",
     view_func=issuecontroller.issueController["get_all_issues"], 
+    methods=["GET"]
+)
+
+issuesprint.add_url_rule(
+    "/issue/issue_info/<int:project_id>/<int:issue_id>",
+    view_func=issuecontroller.issueController["issue"], 
+    methods=["GET","DELETE","PUT"]
+)
+
+issuesprint.add_url_rule(
+    "issue/<int:project_id>/category/<string:category>",
+    view_func=issuecontroller.issueController["fetch_category_issue"], 
+    methods=["GET"]
+)
+
+issuesprint.add_url_rule(
+    "issue/<int:project_id>/team/<string:team>",
+    view_func=issuecontroller.issueController["fetch_team_issue"], 
     methods=["GET"]
 )
