ac3b44db3e7f584652ca7e0acadef2c010b47baf
M-A-D-A-R-A <andoriyanishant@gmail.com>
2022-06-23 17:16:34 +0530

    flutter version upgrade to 3.0

--- labellab_mobile/android/app/src/main/kotlin/org/scorelab/labellab_mobile/MainActivity.kt
+++ labellab_mobile/android/app/src/main/kotlin/org/scorelab/labellab_mobile/MainActivity.kt
@@ -1,13 +1,8 @@
 package org.scorelab.labellab_mobile
 
-import android.os.Bundle
 
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
+
 
 class MainActivity: FlutterActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegistrant.registerWith(this)
-  }
 }
