0195e3daabfabe1fc61a08d1fb99e617c5fefa1f
vaishnavi-gupta18 <vaishnavi_g@me.iitr.ac.in>
2022-08-14 00:36:59 +0530

    Add issue analytics frontend

--- labellab-client/src/reducers/analytics.js
+++ labellab-client/src/reducers/analytics.js
@@ -4,12 +4,16 @@ import {
   ANALYTICS_TIME_LABEL_SUCCESS,
   ANALYTICS_COUNT_LABEL_FAILURE,
   ANALYTICS_COUNT_LABEL_REQUEST,
-  ANALYTICS_COUNT_LABEL_SUCCESS
+  ANALYTICS_COUNT_LABEL_SUCCESS,
+  ISSUE_ANALYTICS_FAILURE,
+  ISSUE_ANALYTICS_REQUEST,
+  ISSUE_ANALYTICS_SUCCESS
 } from '../constants/index'
 const intialState = {
   isfetching: false,
   timeData: {},
-  countData: {}
+  countData: {},
+  issueData: {}
 }
 const analytics = (state = intialState, action) => {
   switch (action.type) {
@@ -41,6 +45,22 @@ const analytics = (state = intialState, action) => {
         countData: action.payload
       }
     case ANALYTICS_COUNT_LABEL_FAILURE:
+      return {
+        ...state,
+        isfetching: false
+      }
+    case ISSUE_ANALYTICS_REQUEST:
+      return {
+        ...state,
+        isfetching: true
+      }
+    case ISSUE_ANALYTICS_SUCCESS:
+      return {
+        ...state,
+        isfetching: false,
+        issueData: action.payload
+      }
+    case ISSUE_ANALYTICS_FAILURE:
       return {
         ...state,
         isfetching: false
