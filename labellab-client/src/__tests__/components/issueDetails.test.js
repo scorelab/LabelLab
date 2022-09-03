dc8e847d410b7d95753a89fc0a99a57ce40a7026
vaishnavi-gupta18 <vaishnavi_g@me.iitr.ac.in>
2022-09-03 18:51:31 +0530

    Add test data in constants and fix issueDetails bug

--- labellab-client/src/__tests__/components/issueDetails.test.js
+++ labellab-client/src/__tests__/components/issueDetails.test.js
@@ -3,6 +3,7 @@ import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16';
 import { testStore, findByTestAttr } from '../../utils/TestUtils'
 import IssueDetails from '../../components/project/issue/issueDetails'
+import {issues, logs, projects, user, labels} from '../../constants/test'
 
 Enzyme.configure({
     adapter: new Adapter(),
@@ -20,141 +21,6 @@ const setUp = (initialState = {}, props = {}) => {
 describe('Issues Details Component', () => {
 
     let wrapper
-    const issues = {
-        currentIssue: {
-            "assigneeId": 1,
-            "category": "general",
-            "comments": [
-                {
-                    "body": "hey",
-                    "id": 1,
-                    "issue_id": 1,
-                    "thumbnail": "https://react.semantic-ui.com/images/avatar/large/elliot.jpg",
-                    "timestamp": "2022-07-21 20:42:27",
-                    "user_id": 1,
-                    "username": "vaishnavi"
-                }
-            ],
-            "createdAt": "2022-07-07 13:42:25",
-            "createdBy": 1,
-            "description": "Frontend Test",
-            "dueDate": "2022-07-21 04:55:00",
-            "entityId": 1,
-            "entityType": "label",
-            "id": 1,
-            "priority": "Critical",
-            "projectId": 1,
-            "status": "Closed",
-            "teamId": 1,
-            "title": "Frontend Test",
-            "updatedAt": "2022-08-11 18:53:51"
-        },
-        issuesActions: {
-            isFetching: false,
-            isDeleting: false,
-            isPosting: false,
-            isUpdating: false,
-            errors: ''
-        },
-        comments: [
-            {
-                "body": "hey",
-                "id": 1,
-                "issue_id": 1,
-                "thumbnail": "https://react.semantic-ui.com/images/avatar/large/elliot.jpg",
-                "timestamp": "2022-07-21 20:42:27",
-                "user_id": 1,
-                "username": "John Doe"
-            }
-        ]
-    }
-    const logs = {
-        logs: [
-            {
-                "category": "general",
-                "entity_id": 1,
-                "entity_type": "issue",
-                "id": 1,
-                "message": "Issue Frontend Test has been created",
-                "project_id": 1,
-                "timestamp": "Thu, 11 Aug 2022 18:53:51 GMT",
-                "user_id": 1,
-                "username": "John Doe"
-            }
-        ],
-        logsActions: {
-            "isFetching": false
-        }
-    }
-    const projects = {
-        currentProject: {
-            projectId: 1,
-            members: [
-                {
-                    "email": "john@gmail.com",
-                    "name": "John doe",
-                    "project_id": 1,
-                    "team_id": 1,
-                    "team_name": "admin",
-                    "team_role": "admin",
-                    "user_id": 1
-                },
-            ],
-            roles: [
-                "admin"
-            ],
-            teams: [
-                {
-                    "id": 1,
-                    "project_id": 1,
-                    "role": "admin",
-                    "team_members": [
-                        {
-                            "team_id": 1,
-                            "user_id": 2
-                        },
-                        {
-                            "team_id": 1,
-                            "user_id": 1
-                        }
-                    ],
-                    "team_name": "admin"
-                },
-            ]
-        }
-    }
-    const user = {
-        users: [
-            {
-                "email": "john@gmail.com",
-                "id": 1,
-                "name": "John doe",
-                "thumbnail": "https://react.semantic-ui.com/images/avatar/large/elliot.jpg",
-                "username": "John Doe"
-            }
-        ],
-        userDetails: {
-            "id": 1,
-            "name": "John doe",
-            "email": "john@gmail.com",
-            "username": "John Doe",
-            "thumbnail": "https://react.semantic-ui.com/images/avatar/large/elliot.jpg",
-            "profileImage": "https://react.semantic-ui.com/images/avatar/large/elliot.jpg"
-        }
-    }
-    const labels = {
-        labels: [
-            {
-                "count": 0,
-                "created_at": "2022-07-08 14:41:15",
-                "id": 1,
-                "label_name": "Label 1",
-                "label_type": "bbox",
-                "labeldata": {},
-                "project_id": 1
-            }
-        ]
-    }
     beforeEach(() => {
         const initialState = {
             issues: issues,
--- labellab-client/src/__tests__/components/issues.test.js
+++ labellab-client/src/__tests__/components/issues.test.js
@@ -3,6 +3,7 @@ import Enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16';
 import { testStore, findByTestAttr } from '../../utils/TestUtils'
 import ProjectIssues from '../../components/project/issue/issues'
+import {issues, logs, projects, user, labels} from '../../constants/test'
 
 Enzyme.configure({
     adapter: new Adapter(),
@@ -20,92 +21,13 @@ const setUp = (initialState = {}, props = {}) => {
 describe('Issues Listing Component', () => {
 
     let wrapper
-    const issues = {
-        issues: {
-            "items": [
-                {
-                    "assignee_id": null,
-                    "category": "labels",
-                    "created_at": "2022-08-13 18:30:12",
-                    "created_by": 2,
-                    "description": "test issue 1",
-                    "due_date": null,
-                    "entity_id": null,
-                    "entity_type": null,
-                    "id": 1,
-                    "priority": "Low",
-                    "project_id": 1,
-                    "status": "Done",
-                    "team_id": null,
-                    "title": "test issue 1",
-                    "updated_at": "2022-08-13 18:30:12"
-                },
-                {
-                    "assignee_id": null,
-                    "category": "models",
-                    "created_at": "2022-08-13 18:29:37",
-                    "created_by": 2,
-                    "description": "test issue 2",
-                    "due_date": null,
-                    "entity_id": null,
-                    "entity_type": null,
-                    "id": 2,
-                    "priority": "Low",
-                    "project_id": 1,
-                    "status": "In Progress",
-                    "team_id": null,
-                    "title": "test issue 2",
-                    "updated_at": "2022-08-13 18:29:37"
-                }
-            ],
-            "page": 1,
-            "perPage": 6,
-            "total": 2
-        },
-        teams: [
-            {
-                "key": 1,
-                "value": 1,
-                "text": "admin"
-            },
-            {
-                "key": 2,
-                "value": 2,
-                "text": "images"
-            }
-        ],
-        members: [
-            {
-                "email": "john@gmail.com",
-                "name": "John doe",
-                "project_id": 1,
-                "team_id": 2,
-                "team_name": "images",
-                "team_role": "images",
-                "user_id": 1
-            },
-        ],
-        users: [
-            {
-                "email": "john@gmail.com",
-                "id": 1,
-                "name": "John doe",
-                "thumbnail": "https://react.semantic-ui.com/images/avatar/large/elliot.jpg",
-                "username": "John Doe"
-            }
-        ],
-        projectId: 1,
-        issuesActions: {
-            isFetching: false,
-            isDeleting: false,
-            isPosting: false,
-            isUpdating: false,
-            errors: ''
-        }
-    }
     beforeEach(() => {
         const initialState = {
-            issues: issues
+            issues: issues,
+            logs: logs,
+            projects: projects,
+            user: user,
+            labels: labels
         }
         wrapper = setUp(initialState);
     });
