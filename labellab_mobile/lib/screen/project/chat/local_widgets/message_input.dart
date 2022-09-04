61ab06d40f8f4d2ae96a078d763d97a7446af6c3
M-A-D-A-R-A <andoriyanishant@gmail.com>
2022-09-04 20:38:36 +0530

    text form feild padding

--- labellab_mobile/lib/screen/project/chat/local_widgets/message_input.dart
+++ labellab_mobile/lib/screen/project/chat/local_widgets/message_input.dart
@@ -20,7 +20,6 @@ class _MessageInputState extends State<MessageInput> {
   List<Map<String, dynamic>> mentiondata = [];
 
   bool isDataLoading = true;
-  // String nonHashvalue = "";
   Timer? _debounce;
   GlobalKey<FlutterMentionsState> key = GlobalKey<FlutterMentionsState>();
 
@@ -41,88 +40,90 @@ class _MessageInputState extends State<MessageInput> {
                 minHeight: 50,
                 maxHeight: 100,
               ),
-              child: FlutterMentions(
-                onEditingComplete: () {
-                  widget.onSubmit; 
-                },
-                onMentionAdd: (Map<String, dynamic> map) {},
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: "Send a message",
-                ),
-                style: const TextStyle(fontSize: 16, color: Colors.black),
-                onSearchChanged: (String trigger, String value) {
-                  if (trigger == "@") {
-                    if (_debounce?.isActive ?? false) _debounce!.cancel();
-                    _debounce = Timer(const Duration(milliseconds: 200), () {
-                      setState(() {
-                        widget.list!.forEach((element) {
-                          mentiondata.add({
-                            "username": element.username,
-                            "id": element.id,
-                            "name": element.name,
-                            "thumbnail": element.thumbnail,
-                            "display": element.username ?? ""
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10),
+                child: FlutterMentions(
+                  onEditingComplete: () {
+                    widget.onSubmit(isDataLoading); 
+                  },
+                  onMentionAdd: (Map<String, dynamic> map) {},
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Send a message",
+                  ),
+                  style: const TextStyle(fontSize: 16, color: Colors.black),
+                  onSearchChanged: (String trigger, String value) {
+                    if (trigger == "@") {
+                      if (_debounce?.isActive ?? false) _debounce!.cancel();
+                      _debounce = Timer(const Duration(milliseconds: 200), () {
+                        setState(() {
+                          widget.list!.forEach((element) {
+                            mentiondata.add({
+                              "username": element.username,
+                              "id": element.id,
+                              "name": element.name,
+                              "thumbnail": element.thumbnail,
+                              "display": element.username ?? ""
+                            });
                           });
                         });
                       });
-                      print(mentiondata);
-                    });
-                  }
-                },
-                key: key,
-                maxLines: 5,
-                minLines: 1,
-                suggestionPosition: SuggestionPosition.Top,
-                mentions: [
-                  Mention(
-                      trigger: '@',
-                      disableMarkup: false,
-                      markupBuilder: ((trigger, mention, id) {
-                        return "@[__${mention}__]";
-                      }),
-                      style: const TextStyle(color: Colors.blue),
-                      data: mentiondata.reversed.toList(),
-                      matchAll: true,
-                      suggestionBuilder: (data) {
-                        return Container(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Row(
-                            children: <Widget>[
-                              CircleAvatar(
-                                backgroundColor: Colors.black12,
-                                radius: 25,
-                                child: ClipOval(
-                                  child: Image(
-                                    width: 60,
-                                    height: 60,
-                                    image: CachedNetworkImageProvider(
-                                        data['thumbnail']),
-                                    fit: BoxFit.cover,
+                    }
+                  },
+                  key: key,
+                  maxLines: 5,
+                  minLines: 1,
+                  suggestionPosition: SuggestionPosition.Top,
+                  mentions: [
+                    Mention(
+                        trigger: '@',
+                        disableMarkup: false,
+                        markupBuilder: ((trigger, mention, id) {
+                          return "@[__${mention}__]";
+                        }),
+                        style: const TextStyle(color: Colors.blue),
+                        data: mentiondata.reversed.toList(),
+                        matchAll: true,
+                        suggestionBuilder: (data) {
+                          return Container(
+                            padding: const EdgeInsets.all(10.0),
+                            child: Row(
+                              children: <Widget>[
+                                CircleAvatar(
+                                  backgroundColor: Colors.black12,
+                                  radius: 25,
+                                  child: ClipOval(
+                                    child: Image(
+                                      width: 60,
+                                      height: 60,
+                                      image: CachedNetworkImageProvider(
+                                          data['thumbnail']),
+                                      fit: BoxFit.cover,
+                                    ),
                                   ),
                                 ),
-                              ),
-                              const SizedBox(
-                                width: 20.0,
-                              ),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: <Widget>[
-                                  Text(data['name']),
-                                  Text('@${data['username']}'),
-                                ],
-                              )
-                            ],
-                          ),
-                        );
-                      }),
-                ],
+                                const SizedBox(
+                                  width: 20.0,
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(data['name']),
+                                    Text('@${data['username']}'),
+                                  ],
+                                )
+                              ],
+                            ),
+                          );
+                        }),
+                  ],
+                ),
               ),
             ),
           ),
           IconButton(
             onPressed: () {
-              widget.onSubmit(widget.controller.text);
+              widget.onSubmit(key.currentState!.controller!.text);
               widget.controller.clear();
             },
             icon: Icon(
